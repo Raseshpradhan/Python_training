5e973c2d632a7fba247f2f49aff503d299806f72
F055LBLR <280065@ust.com>
2024-06-27 11:21:22 +0530

    1st update in operator_assignment,py

--- operators_assignments.py
+++ operators_assignments.py
@@ -10,3 +10,64 @@ print([1, 2] + [3, 4])
 #o/p is - [1, 2, 3, 4]
 print('Hi' + [1, 2])
 #o/p is -can only concatenate str (not "list") to str'''
+# What will be the output of following programs
+print((1, 2, 3) * 4)
+print(8 // 3)
+print(21 % 6)
+
+# What will be the output of following programs
+a = 7
+a /= 2
+print(a)  # 3.5
+b = 5
+print(b > 5 and b < 10)  # Also write the short form for this expression
+print(5 < b < 10)
+
+# Write the output of following programs
+print(4 and 6)
+print(1 or 5)
+print(not 7)
+
+# what is the output of following programs
+print(2 not in [3, 4, 5, 2, 9])
+print([1, 2, 5, (1, 2)] in ['hi', 3, 4, 5, 2, 9, [1, 2, 5, (1, 2)]])
+
+# what is the output of following programs
+a = 400.30123
+b = 400.30123
+print(a is b)
+
+i = 23
+j = 23
+print(i is j)
+print(i == j)
+
+a1 = 999
+b1 = 999
+print(a1 is b1)
+print(a1 == b1)
+
+y = 4
+z = -4
+print(y == z)
+
+s1 = 4096 * 'a'
+s2 = 4096 * 'a'
+print(s1 is s2)
+
+# write the output of following programs
+a = 2
+b = 11
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~b)
+print(a << 3)
+print(b >> 2)
+
+# write the output of following programs
+print(+-4)
+print(--4)
+a = [1, 2, 3]
+print(2 * a[1] << 2 > 8 and 9)
+print(2 * 3 - 3 ** 2 ** 1 & 5 // 2 + (4 + 2) or 5)
