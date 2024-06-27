f312883c8bba2c733111500ccc0d74828cd76e02
F055LBLR <280065@ust.com>
2024-06-27 11:13:22 +0530

    1st commit

---
+++ operators_assignments.py
@@ -0,0 +1,12 @@
+'''# What will be the output of following statements
+print({1, 2, 3, 5} - {3, 4, 7, 8})
+#o/p is -{1, 2, 5}
+print({1, 2, 3, 5} + {3, 4, 7, 8})
+#o/p is -unsupported operand type(s) for +: 'set' and 'set'
+# What will be the output of following programs'''
+'''print('Hi ' + 'Anil')
+#o/p is - Hi Anil
+print([1, 2] + [3, 4])
+#o/p is - [1, 2, 3, 4]
+print('Hi' + [1, 2])
+#o/p is -can only concatenate str (not "list") to str'''
